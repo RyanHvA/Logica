de7c3cb3c0982f649b222792a3ed27a9ad307525
Ryan <ryan.j.z@hotmail.com>
2019-09-03 22:47:10 +0200

    updated to working app

--- app/src/main/java/com/example/logica/MainActivity.kt
+++ app/src/main/java/com/example/logica/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.logica
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,18 +14,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
-        //set button listener
+        btnSubmit.setOnClickListener {
+            onSubmitClick()
+        }
     }
 
-    private fun answerChecker() {
-
+    private fun onSubmitClick() {
+        if (answerVal1.toString() == getString(R.string.t)
+            && answerVal2.toString() == getString(R.string.f)
+            && answerVal3.toString() == getString(R.string.f)
+            && answerVal4.toString() == getString(R.string.f)) {
+            onAnswerCorrect()
+        } else { onAnswerIncorrect()
+        }
     }
 
     private fun onAnswerCorrect() {
-        Toast.makeText(this, getString(R.string.textIncorrect), Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, getString(R.string.textCorrect), Toast.LENGTH_SHORT).show()
     }
 
     private fun onAnswerIncorrect() {
-        Toast.makeText(this, getString(R.string.textCorrect), Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, getString(R.string.textIncorrect), Toast.LENGTH_SHORT).show()
     }
 }
